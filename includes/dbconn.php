cf9b447d14c5570bf0c61cfccbf8515d90a0b8d1
Akash-Dinkar-Bhujbal <akashbhujbal97@gmail.com>
2022-04-14 20:58:07 +0530

    third commit

--- includes/dbconn.php
+++ includes/dbconn.php
@@ -1,10 +1,10 @@
 
 <?php 
 
-$host="localhost"; // Host name 
-$username="root"; // Mysql username 
-$password="1234"; // Mysql password 
-$db_name="matrimony"; // Database name 
+$host="remotemysql.com"; // Host name 
+$username="sBWHJW4Z1G"; // Mysql username 
+$password="7PgBf3FWyn"; // Mysql password 
+$db_name="sBWHJW4Z1G"; // Database name 
 
 // Connect to server and select databse.
 $conn=mysqli_connect("$host", "$username", "$password")or die("cannot connect"); 
